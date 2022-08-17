1bca25b21907b9ae41dd95e81920aaf0866f485c
Lagaras Stelios <50661518+lagamura@users.noreply.github.com>
2022-08-17 18:47:30 +0300

    CORRECTED create engine faulse argument

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 SQLALCHEMY_DATABASE_URI = "postgresql://zkfhojomydbtyj:126966877ba800983dc6fcfd7557e625a35c01496c17e2f794c77a21119b64d0@ec2-54-228-218-84.eu-west-1.compute.amazonaws.com:5432/d59046ahv6dkcm"
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URI, future=True,connect_args={"check_same_thread": False}) #echo can be used for logging
+engine = create_engine(SQLALCHEMY_DATABASE_URI, future=True) #echo can be used for logging
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
